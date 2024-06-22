2a62ded262043ab8489b77096b2038d5e302af87
nikhil <kumarnikil220@gmail.com>
2024-06-22 20:07:21 +0530

    data stored to database successfully by using hibernate

--- src/main/java/com/nikhil/DemoHibernate/App.java
+++ src/main/java/com/nikhil/DemoHibernate/App.java
@@ -1,6 +1,7 @@
 package com.nikhil.DemoHibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class App 
@@ -15,6 +16,8 @@ public class App
         Configuration con=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Alien.class); //By default file ka name yehi hota hai ,to do ya mtt do file name do it's your choice but in case of different file name ,it is mandatory to specify the name of the file 
         SessionFactory sf=con.buildSessionFactory();
         Session session=sf.openSession();
+        Transaction tx=session.beginTransaction();
         session.save(nik);
+        tx.commit();
     }
 }
