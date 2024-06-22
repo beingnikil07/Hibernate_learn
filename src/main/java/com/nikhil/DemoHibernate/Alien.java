09350004524787a8e26ff11156cc06de7c413ccc
beingnikil07 <kumarnikil220@gmail.com>
2024-06-22 15:27:59 +0530

    initializing the object data

--- src/main/java/com/nikhil/DemoHibernate/Alien.java
+++ src/main/java/com/nikhil/DemoHibernate/Alien.java
@@ -2,27 +2,33 @@ package com.nikhil.DemoHibernate;
 
 public class Alien  //POJO
 {
-	private int aid;
-	private String aname;
-	private String color;
-	public int getAid() {
-		return aid;
-	}
-	public void setAid(int aid) {
-		this.aid = aid;
-	}
-	public String getAname() {
-		return aname;
-	}
-	public void setAname(String aname) {
-		this.aname = aname;
-	}
-	public String getColor() {
-		return color;
-	}
-	public void setColor(String color) {
-		this.color = color;
-	}
-	
-	
+    private int aid;
+    private String aname;
+    private String color;
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
 }
--- src/main/java/com/nikhil/DemoHibernate/App.java
+++ src/main/java/com/nikhil/DemoHibernate/App.java
@@ -1,13 +1,21 @@
 package com.nikhil.DemoHibernate;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
     public static void main( String[] args )
     {
-    
+        Alien nik=new Alien();
+        nik.setAid(10);
+        nik.setAname("Nikhil Kumar Rana");
+        nik.setColor("black");
+
+        Configuration con=new Configuration();
+        SessionFactory sf=con.buildSessionFactory();
+        Session session=sf.openSession();
+        session.save(nik);
     }
 }
