69ee2a5822b5748c4b14f8d7c458b6b026be1961
nikhil <kumarnikil220@gmail.com>
2024-06-22 18:13:00 +0530

    configure file  to making the connection with mysql

--- src/main/java/com/nikhil/DemoHibernate/App.java
+++ src/main/java/com/nikhil/DemoHibernate/App.java
@@ -1,5 +1,4 @@
 package com.nikhil.DemoHibernate;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -12,7 +11,7 @@ public class App
         nik.setAid(10);
         nik.setAname("Nikhil Kumar Rana");
         nik.setColor("black");
-
+        
         Configuration con=new Configuration();
         SessionFactory sf=con.buildSessionFactory();
         Session session=sf.openSession();
