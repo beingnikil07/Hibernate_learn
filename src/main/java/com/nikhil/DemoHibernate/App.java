43f4f8e7e61987a276df3eb974baba2974970302
nikhil <kumarnikil220@gmail.com>
2024-06-23 14:42:48 +0530

    show sql query

--- src/main/java/com/nikhil/DemoHibernate/App.java
+++ src/main/java/com/nikhil/DemoHibernate/App.java
@@ -5,16 +5,15 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class App 
 {
     public static void main( String[] args )
     {
         Alien nik=new Alien();
-        
-        nik.setAid(12);
-        nik.setAname("anamika");
-        nik.setColor("blue");
-        
+        nik.setAid(13);
+        nik.setAname("rakhi");
+        nik.setColor("cyan");
         
         Configuration con=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Alien.class); //By default file ka name yehi hota hai ,to do ya mtt do file name do it's your choice but in case of different file name ,it is mandatory to specify the name of the file 
         //ServiceRegistry is a Interface ,we can't create its object 
