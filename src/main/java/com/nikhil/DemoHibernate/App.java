16e60be4de39b7b9bfb8d5ce40f03e427ce87500
nikhil <kumarnikil220@gmail.com>
2024-06-23 14:31:57 +0530

    removing the deprecated methods by replacing the deprecated method with new methods

--- src/main/java/com/nikhil/DemoHibernate/App.java
+++ src/main/java/com/nikhil/DemoHibernate/App.java
@@ -3,20 +3,23 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 public class App 
 {
     public static void main( String[] args )
     {
         Alien nik=new Alien();
         
-        nik.setAid(11);
-        nik.setAname("Ritik");
+        nik.setAid(12);
+        nik.setAname("anamika");
         nik.setColor("blue");
         
         
         Configuration con=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Alien.class); //By default file ka name yehi hota hai ,to do ya mtt do file name do it's your choice but in case of different file name ,it is mandatory to specify the name of the file 
-        SessionFactory sf=con.buildSessionFactory();
+        //ServiceRegistry is a Interface ,we can't create its object 
+        ServiceRegistry reg=new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+        SessionFactory sf=con.buildSessionFactory(reg);
         Session session=sf.openSession();
         Transaction tx=session.beginTransaction();
         session.save(nik);
