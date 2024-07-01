38ff9090763390ad7f10290e45a69d490024ee00
nikhil <kumarnikil220@gmail.com>
2024-07-02 00:25:23 +0530

    fetching data from database using Query method

--- src/main/java/com/nikhil/DemoHibernate/App.java
+++ src/main/java/com/nikhil/DemoHibernate/App.java
@@ -1,4 +1,5 @@
 package com.nikhil.DemoHibernate;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -23,12 +24,12 @@ public class App
         ServiceRegistry reg=new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
         SessionFactory sf=con.buildSessionFactory(reg);
         
-        
-        
         Session session1=sf.openSession();
         session1.beginTransaction();
+        //Fetching data from database using query
+        Query q1=session1.createQuery("from Alien where aid=1");
         
-        a=(Alien) session1.get(Alien.class,01);
+        a=(Alien)q1.uniqueResult();
         System.out.println(a);
         
         session1.getTransaction().commit(); 
@@ -36,8 +37,9 @@ public class App
         
         Session session2=sf.openSession();
         session2.beginTransaction();
-       
-        a=(Alien) session2.get(Alien.class,01);
+        Query q2=session2.createQuery("from Alien where aid=1");
+        
+        a=(Alien)q2.uniqueResult();
         System.out.println(a);
         
         session2.getTransaction().commit();
